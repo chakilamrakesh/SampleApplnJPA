f86fae09a57a6f6bb36997f6377ae9ef5442e6d5
Anil Kumar Mote <am18164@corp.ojas-it.com>
2020-10-06 19:23:57 +0530

    Zuul API gateway and Eureka server

---
+++ APIGateway_POC/src/main/java/com/ojas/zool/apigateway/ApiGatewayPocApplication.java
@@ -0,0 +1,30 @@
+package com.ojas.zool.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+@EnableSwagger2
+public class ApiGatewayPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayPocApplication.class, args);
+	}
+
+	@Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.ojas.poc.controller")).build();
+    }
+}
---
+++ APIGateway_POC/src/test/java/com/ojas/zool/apigateway/ApiGatewayPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojas.zool.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
